40af8bb132c8956d76dcce91ba95400d2bffad92
Laurenda Edorh <laurenda98@gmail.com>
2023-09-14 00:49:00 +0200

    les responsive form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-let small_menu = document.querySelector(".small_menu");
-var menu = document.querySelector(".menu");
+let small_menu = document.querySelector('.small_menu');
+
+var menu = document.querySelector('.menu');
 
 small_menu.onclick = function () {
+  
   small_menu.classList.toggle('active');
   menu.classList.toggle('small');
  
